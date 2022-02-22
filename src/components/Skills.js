1f498b1112253277535d346428af3c738c697f18
boltex33 <laca_rey@yahoo.com>
2022-02-22 18:56:39 +0200

    amogus3

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 function Skills() {
   const [toggle, setToggle] = useState(1);
